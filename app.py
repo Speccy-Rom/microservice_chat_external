0e800079080ab9ef17584741f2504c8254f5b638
speccy-rom <speccy.rom@gmail.com>
2022-01-11 23:42:10 +0300

    fixed: create loop and tasks in main module

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import asyncio
 from starlette.applications import Starlette
 from starlette.routing import Mount
 from chat.urls import routes as chat_routes
 from main.urls import routes as main_routes
-from settings import DEBUG
+from consumer.subscriptions import consumer_subscriptions
 from crontab import one_minute_message
+from settings import DEBUG
 
 
 routes = [
@@ -14,6 +16,8 @@ routes = [
 
 class AmqpHttpServer(Starlette):
     def __init__(self, *args, **kwargs):
+        loop = asyncio.get_event_loop()  # получаем event loop
+        loop.create_task(consumer_subscriptions())  # создаем таск
         one_minute_message.start()
         super().__init__(*args, **kwargs)
 
